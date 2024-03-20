bd29dd1b00f76ccab8c189e1cdf2d1a6427012a8
Caio Alonso da Costa <caioalonso@gmail.com>
2024-03-20 21:27:55 +0100

    Update mux3_unit.v

--- src/mux3_unit.v
+++ src/mux3_unit.v
@@ -1,1 +1,22 @@
+`timescale 1ns / 1ps
 
+module mux3_unit (a, b, sel, out);
+
+  input [9:0] a;
+  input [9:0] b;
+  input sel;
+
+  output [9:0] out;
+
+  supply0 [9:0] zero;
+  reg [9:0] out;
+
+  always @(a or b or sel) begin
+    case (sel)
+      1'b0 : out = a; 
+      1'b1 : out = b;
+      default out = zero;
+    endcase
+  end
+
+endmodule
