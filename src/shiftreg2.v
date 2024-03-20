3e2bb6c5c2f09a562112460f9c10200f9b1436b3
Caio Alonso da Costa <caioalonso@gmail.com>
2024-03-20 21:47:24 +0100

    Update shiftreg2.v

--- src/shiftreg2.v
+++ src/shiftreg2.v
@@ -14,6 +14,7 @@ module shiftreg2 (en, rstb, clk, rst_mmm_i, ld_a, rjo, reg_rji);
 
   wire clk_i;
   wire rstb_i;
+  wire en_i;
   wire rst_mmm_int;
   wire rst_mmm_int_buf;
 
@@ -22,12 +23,13 @@ module shiftreg2 (en, rstb, clk, rst_mmm_i, ld_a, rjo, reg_rji);
   buf U0 (rstb_i, rstb);
   buf U1 (clk_i, clk);
   buf U2 (rst_mmm_int_buf, rst_mmm_int);
+  buf U3 (en_i, en);
 
   always @(negedge(rst_mmm_int_buf) or posedge(clk_i)) begin
     if (!rst_mmm_int_buf) begin 
       reg_rji <= {10{1'b0}};
     end else begin 
-      if (en) begin
+      if (en_i) begin
         if (ld_a == 1'b1) begin 
           reg_rji <= {10{1'b0}};
         end else begin 
