4ef0c2a7cf53dabca239487fd49576d6b932c124
Caio Alonso da Costa <caioalonso@gmail.com>
2024-03-26 22:59:06 +0000

    More logic again.

--- src/tt_um_calonso88_rsa_top.v
+++ src/tt_um_calonso88_rsa_top.v
@@ -16,22 +16,23 @@ module tt_um_calonso88_rsa_top (
   input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out[2:0]  = 0;
-  assign uo_out[6:4]  = 0;
-  assign uio_out[7:0] = 0;
-  assign uio_oe  = 0;
-
   // SPI PINs
   wire spi_cs_n;
   wire spi_clk;
   wire spi_miso;
   wire spi_mosi;
-  assign spi_cs_n = ui_in[0];
-  assign spi_clk  = ui_in[1];
-  assign spi_mosi = ui_in[2];
-  assign uo_out[3] = spi_miso;
+  // Auxiliars
+  wire start;
+  wire start_cmd;
+  wire stop_cmd;
+  wire stop_pos_edge;
+  wire rsa_eoc;
+  wire en_rsa;
+  wire rst_rsa;
+  wire irq;
+  wire eoc_mem;
 
+  // SPIREG
   localparam integer ADDR_WIDTH = 3;
   localparam integer REG_WIDTH = 8;
 
@@ -39,11 +40,50 @@ module tt_um_calonso88_rsa_top (
   wire [REG_WIDTH-1:0] reg_data_i, reg_data_o;
   wire reg_data_o_vld;
   wire [REG_WIDTH-1:0] status;
-  reg [REG_WIDTH-1:0] mem [0:(2**ADDR_WIDTH-1)];
-  wire rsa_eoc;
-  wire [REG_WIDTH-1:0] result;
+  reg  [REG_WIDTH-1:0] mem [0:(2**ADDR_WIDTH-1)];
+  
+  // RSA P, E, M, Const and C
+  wire [REG_WIDTH-1:0] P;
+  wire [REG_WIDTH-1:0] E;
+  wire [REG_WIDTH-1:0] M;
+  wire [REG_WIDTH-1:0] Const;
+  wire [REG_WIDTH-1:0] C;
 
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out[2:0]  = 0;
+  assign uo_out[7:5]  = 0;
+  assign uio_out[7:0] = 0;
+  assign uio_oe  = 0;
 
+  // Input ports
+  assign spi_cs_n = ui_in[0];
+  assign spi_clk  = ui_in[1];
+  assign spi_mosi = ui_in[2];
+  assign start = ui_in[3]; /////TBD TBD TBD
+
+  // Output ports
+  assign uo_out[3] = spi_miso;
+  assign uo_out[4] = irq; /////TBD TBD TBD
+
+  // Map registers to RSA signals
+  assign P = mem[2];
+  assign E = mem[3];
+  assign M = mem[4];
+  assign Const = mem[5];
+  assign start_cmd = mem[1][0];
+  assign stop_cmd = mem[1][1];
+  
+  //Status signals
+  assign status[0] = irq;
+  assign status[1] = eoc_mem;
+
+  // Controller
+  rsa_en_logic rsa_en_logic_i (.rstb(rst_n), .clk(clk), .ena(ena), .start(start), .start_cmd(start_cmd), .stop_cmd(stop_cmd), .eoc_int(rsa_eoc), .en_rsa(en_rsa), .rst_rsa(rst_rsa), .eoc(irq), .eocp(eoc_mem));
+
+  // RSA Instance
+  rsa_unit #(.WIDTH(REG_WIDTH)) rsa_i (.en(en_rsa), .rstb(rst_rsa), .clk(clk), .P(P), .E(E), .M(M), .Const(Const), .eoc(rsa_eoc), .C(C));
+
+  // Serial interface
   spireg #(
     .ADDR_W(ADDR_WIDTH),
     .REG_W(REG_WIDTH)
@@ -63,11 +103,8 @@ module tt_um_calonso88_rsa_top (
     .fastcmd_vld()
   );
 
-  //register read access
-  assign reg_data_i = mem[reg_addr];
-
-  //status signals
-  assign status = mem[0][7:0];
+  // Register read access
+  assign reg_data_i = (reg_addr == 0) ? status : mem[reg_addr];
 
   //register write and fastcmd access
   integer i;
@@ -80,8 +117,8 @@ module tt_um_calonso88_rsa_top (
       if (reg_data_o_vld) begin
         //register write access
         mem[reg_addr] <= reg_data_o;
-      end else if (rsa_eoc) begin
-        mem[6] <= result;
+      end else if (eoc_spi) begin
+        mem[6] <= C;
       end
     end
   end
@@ -90,22 +127,14 @@ module tt_um_calonso88_rsa_top (
 In this example:
 *  total 8 registers available, each 8bits wide
 
-   Addr 0 - Status, Bit0 (EOC);
-   Addr 1 - Actions, Bit0 (Start)
+   Addr 0 - Read Status, Write is Spare register
+   Addr 1 - Actions, Bit0 (Start), Bit1 (Stop)
    Addr 2 - P;
    Addr 3 - E;
    Addr 4 - M;
    Addr 5 - Const;
    Addr 6 - C;
-   Addr 7 - Spare; 
+   Addr 7 - Spare;
 */
-
-assign uo_out[7] = rsa_eoc;
-//assign result[7:4] = '0;
-
-// Instance
-//rsa_unit rsa_i (.en(ena), .rstb(rst_n), .clk(clk), .P(ui_in), .E(ui_in), .M(ui_in), .Const(ui_in), .eoc(uio_out[0]), .C(uo_out));
-//rsa_unit #(.WIDTH(4)) rsa_i (.en(ena), .rstb(rst_n), .clk(clk), .P(mem[2][3:0]), .E(mem[3][3:0]), .M(mem[4][3:0]), .Const(mem[5][3:0]), .eoc(rsa_eoc), .C(result[3:0]));
-rsa_unit #(.WIDTH(8)) rsa_i (.en(ena), .rstb(rst_n), .clk(clk), .P(mem[2]), .E(mem[3]), .M(mem[4]), .Const(mem[5]), .eoc(rsa_eoc), .C(result));
-
+  
 endmodule
