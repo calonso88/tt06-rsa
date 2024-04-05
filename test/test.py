313c8f71c5207372902db96d3c6d7ab576face55
Caio Alonso da Costa <caioalonso@gmail.com>
2024-04-05 05:16:56 +0000

    Trial.

--- test/test.py
+++ test/test.py
@@ -17,32 +17,32 @@ def clear_bit(value, bit_index):
 def pull_cs_high(dut):
   #dut.ui_in.value = 0x1
   #dut.ui_in.value = dut.ui_in.value | (0x1)
-  dut.ui_in.value = set_bit(dut.ui_in.value, 0)
+  dut.ui_in.value = set_bit(dut.ui_in.value.integer, 0)
 
 def pull_cs_low(dut):
   #dut.ui_in.value = 0x0
   #dut.ui_in.value = dut.ui_in.value & ~(0x1)
-  dut.ui_in.value = clear_bit(dut.ui_in.value, 0)
+  dut.ui_in.value = clear_bit(dut.ui_in.value.integer, 0)
 
 def spi_clk_high(dut):
   #dut.ui_in.value = 0x2
   #dut.ui_in.value = dut.ui_in.value | (0x2)
-  dut.ui_in.value = set_bit(dut.ui_in.value, 1)
+  dut.ui_in.value = set_bit(dut.ui_in.value.integer, 1)
 
 def spi_clk_low(dut):
   #dut.ui_in.value = 0x0
   #dut.ui_in.value = dut.ui_in.value & ~(0x2)
-  dut.ui_in.value = clear_bit(dut.ui_in.value, 1)
+  dut.ui_in.value = clear_bit(dut.ui_in.value.integer, 1)
 
 def spi_mosi_high(dut):
   #dut.ui_in.value = 0x4
   #dut.ui_in.value = dut.ui_in.value | (0x4)
-  dut.ui_in.value = set_bit(dut.ui_in.value, 2)
+  dut.ui_in.value = set_bit(dut.ui_in.value.integer, 2)
 
 def spi_mosi_low(dut):
   #dut.ui_in.value = 0x0
   #dut.ui_in.value = dut.ui_in.value & ~(0x4)
-  dut.ui_in.value = clear_bit(dut.ui_in.value, 2)
+  dut.ui_in.value = clear_bit(dut.ui_in.value.integer, 2)
 
 def spi_miso_read(dut):
   return get_bit (dut.uo_out.value, 3)
