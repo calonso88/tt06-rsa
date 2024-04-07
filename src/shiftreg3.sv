3e0eea231a72cb3a74d32ce6567c8fc669079a5f
Caio Alonso da Costa <caioalonso@gmail.com>
2024-04-07 09:50:08 +0200

    Update shiftreg3.sv

--- src/shiftreg3.sv
+++ src/shiftreg3.sv
@@ -20,10 +20,10 @@ module shiftreg3 #(parameter int WIDTH = 4) (en, rstb, clk, rst_mmm_i, lock, ld_
       R_i <= '0;
     end else begin
       if (en == 1'b1) begin
-        if ((lock == 1'b1) && (ld_r == 1'b1)) begin
-          R_i <= reg_rji;
-        end else begin
-          if (ld_r == 1'b1) begin
+        if (ld_r == 1'b1) begin
+          if (lock == 1'b1) begin
+            R_i <= reg_rji;
+          end else begin
             R_i <= A;
           end
         end
