ce21bf36060e19881fa94c0655296597b7a028e5
Caio Alonso da Costa <caioalonso@gmail.com>
2024-04-13 11:01:39 +0200

    Update register_crypt.sv

--- src/register_crypt.sv
+++ src/register_crypt.sv
@@ -1,9 +1,9 @@
-module register_crypt #(parameter int WIDTH = 4) (en, rstb, clk, eoc, R_i, C_ex);
+module register_crypt #(parameter int WIDTH = 4) (ena, rstb, clk, load, R_i, C_ex);
 
-  input en;
+  input ena;
   input rstb;
   input clk;
-  input eoc;
+  input load;
   input [WIDTH-1:0] R_i;
   output [WIDTH-1:0] C_ex;
 
@@ -13,8 +13,8 @@ module register_crypt #(parameter int WIDTH = 4) (en, rstb, clk, eoc, R_i, C_ex)
     if (!rstb) begin
       C_ex <= '0;
     end else begin
-      if (en == 1'b1) begin
-        if (eoc == 1'b1) begin
+      if (ena == 1'b1) begin
+        if (load == 1'b1) begin
           C_ex <= R_i;
         end
       end
